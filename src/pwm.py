34d53dc436a140a78ec92a454f3d7faf6a7dcb05
ajparker <ajparker1401@gmail.com>
2021-05-01 12:05:14 -0700

    Added testing code

--- src/pwm.py
+++ src/pwm.py
@@ -6,3 +6,20 @@ from std_msgs.msg import Bool
 from sensor_msgs.msg import Joy
 
 
+def changeSpeed(data):
+  print(data.data)
+
+
+
+
+
+if __name__ == "__main__":
+  GPIO.setmode(GPIO.BOARD)
+  switcher_pin = 32
+  duty_cycle_pin = 33
+  frequency = 1000
+  GPIO.setup(switcher_pin, GPIO.OUT)
+  GPIO.setup(duty_cycle_pin, GPIO.OUT) 
+  pwm = GPIO.PWM(duty_cycle_pin, frequency)
+
+  rospy.Subscriber("joy/joy1", Joy, changeSpeed)
