7cf7df3efbb58f2ab5973ee3177c34811d4558d5
ajgrinds <ajgrinds03@gmail.com>
2021-05-31 11:09:53 -0700

    Removed direction

--- src/pwm.py
+++ src/pwm.py
@@ -12,7 +12,6 @@ class PWM:
         self.frequency = frequency
         self.gpio_setup(GPIO.BOARD)
 
-        self.direction = -1
         self.previousValue = 0
 
     def gpio_setup(self, mode):
@@ -26,12 +25,11 @@ class PWM:
     # Created a 0.1 threshold before changing the speed
         if abs(self.previousValue - data.axes[3]) > 0.1:
             self.pwm.ChangeDutyCycle(abs(data.axes[3]) * 100)
-            self.previousValue = abs(data.axes[3])
+            self.previousValue = data.axes[3]
             # switch the direction if the previous number and this number have different signs using xor
-            if self.direction * data.axes[3] < 0:
-              self.direction *= -1
-              rospy.loginfo('micro_rov: switched direction to {}'.format(self.direction))
-              GPIO.output(self.switcher_pin, self.direction == 1)
+            if self.previousValue * data.axes[3] < 0:
+              rospy.loginfo('micro_rov: switched direction to {}'.format('forwards' if data.axes[3] > 0 else 'backwards'))
+              GPIO.output(self.switcher_pin, data.axes[3] > 0)
 
 
 if __name__ == "__main__":
