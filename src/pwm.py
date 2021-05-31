14d2b44c1b46331f697a172f5a79c59814fa34df
ajgrinds <ajgrinds03@gmail.com>
2021-05-31 11:15:20 -0700

    trying to xor with ints:

--- src/pwm.py
+++ src/pwm.py
@@ -15,6 +15,7 @@ class PWM:
         self.previousValue = 0
 
     def gpio_setup(self, mode):
+        GPIO.setwarnings(False)
         GPIO.setmode(mode)
         GPIO.setup(self.switcher_pin, GPIO.OUT)
         GPIO.setup(self.duty_cycle_pin, GPIO.OUT)
@@ -26,7 +27,7 @@ class PWM:
         if abs(self.previousValue - data.axes[3]) > 0.1:
             self.pwm.ChangeDutyCycle(abs(data.axes[3]) * 100)
             # switch the direction if the previous number and this number have different signs using xor
-            if self.previousValue * data.axes[3] < 0:
+            if int(self.previousValue) ^ int(data.axes[3]) < 0:
               rospy.loginfo('micro_rov: switched direction to {}'.format('forwards' if data.axes[3] > 0 else 'backwards'))
               GPIO.output(self.switcher_pin, int(data.axes[3] > 0))
             self.previousValue = data.axes[3]
@@ -36,3 +37,4 @@ if __name__ == "__main__":
   rospy.init_node("micro_rov")
   rospy.Subscriber("/joy/joy1", Joy, pwm.callback)
   rospy.spin()
+  GPIO.cleanup()
